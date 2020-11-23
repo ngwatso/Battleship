be17edffe04627e637577bafb41345d78843c7ad
Nicholas Watson <ngwatso@Nicholass-iMac.hsd1.mi.comcast.net>
2020-11-23 17:50:44 -0500

    created grids for user and computer

---
+++ src/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const userGrid = document.querySelector('.grid-user');
+	const computerGrid = document.querySelector('.grid-computer');
+	const displayGrid = document.querySelector('.grid-display');
+	const ships = document.querySelectorAll('.ship');
+	const destroyer = document.querySelector('.destroyer-container');
+	const submarine = document.querySelector('.submarine-container');
+	const cruiser = document.querySelector('.cruiser-container');
+	const battleship = document.querySelector('.battleship-container');
+	const carrier = document.querySelector('.carrier-container');
+	const startButton = document.querySelector('#start');
+	const rotateButton = document.querySelector('#rotate');
+	const turnsDisplay = document.querySelector('#whos-go');
+	const infoDisplay = document.querySelector('#info');
+	const userSquares = [];
+	const computerSquares = [];
+
+	const width = 10;
+
+	// ! Create Board
+	function createBoard(grid, squares, width) {
+		for (let i = 0; i < width * width; i++) {
+			const square = document.createElement('div');
+			square.dataset.id = i;
+			grid.appendChild(square);
+			squares.push(square);
+		}
+	}
+
+	createBoard(userGrid, userSquares, width);
+	createBoard(computerGrid, computerSquares, width);
+});
